20f2cd2d4db33f504a3fb7e6a781510ae9f1a3e6
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-02-16 23:52:34 +0100

    header sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 export function Header() {
     return (
-        <header className="bg-custom-blue py-4 shadow-md">
+        <header className="bg-custom-blue py-4 shadow-md sticky top-0 z-50">
             <div className="container flex mx-auto justify-between items-center">
                 <div><img className="w-30" src={logo} alt="" /></div>
                 <nav>
                     <ul className="flex gap-4.5">
                         <li className="text-gray-200 font-medium text-xl hover:text-gray-700 transform hover:scale-110 transition duration-300"><Link to={'/'}>Inicio</Link></li>
-                        <li className="text-gray-200 font-medium text-xl hover:text-gray-700 transform hover:scale-110 transition duration-300"><Link to={'/otro'}>Tienda</Link></li>
+                        <li className="text-gray-200 font-medium text-xl hover:text-gray-700 transform hover:scale-110 transition duration-300"><Link to={'/tienda'}>Tienda</Link></li>
                         <li className="text-gray-200 font-medium text-xl hover:text-gray-700 transform hover:scale-110 transition duration-300">Contacto</li>
                     </ul>
                 </nav>
