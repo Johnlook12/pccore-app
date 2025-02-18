a27bb9e7bc3f15f2216e5e2cfe65167307416513
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-02-18 22:00:03 +0100

    style(CategorySelect): add hover effects

--- src/components/CategorySelect.jsx
+++ src/components/CategorySelect.jsx
@@ -9,7 +9,7 @@ export default function CategorySelect({categorias, onChange}){
 
     return(
         <div className="inline">
-            <select name="category" id="category" className="bg-[#333333]" onChange={handleChange}>
+            <select name="category" id="category" className="bg-[#333333] transition-colors focus:border-blue-400 duration-250 hover:border-blue-400 p-2 border border-gray-900 rounded-lg text-sm" onChange={handleChange}>
                 <option value={0}>Seleccionar categoría</option>
                 {categorias.map((item,index) => (
                     <option key={index} value={item.id}>{item.nombre}</option>
