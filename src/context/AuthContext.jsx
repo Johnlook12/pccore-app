62d4cdfed30ac8580c1d6c7c0a916eb04794b2ce
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-02-20 23:48:47 +0100

    feat: add loading state

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,14 +5,17 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
         const storedUser = localStorage.getItem('user');
         if (storedUser) setUser(JSON.parse(storedUser));
+        setLoading(false);
     }, []);
 
     const login = async (email, password) => {
+        setLoading(true);
         const response = await fetch('http://localhost:3001/login', {
             method: 'POST',
             headers: {
@@ -28,12 +31,13 @@ export function AuthProvider({ children }) {
 
             const tipoResponse = await fetch(`http://localhost:3001/tipousuario/${userData.tipousuario_id}`);
 
-            const tipoUsuario = tipoResponse.json();
+            const tipoUsuario = await tipoResponse.json();
 
             const fullUser = { ...userData, tipoUsuario,token };
 
             setUser(fullUser);
             localStorage.setItem('user', JSON.stringify(fullUser));
+            setLoading(false);
             return true;
         }
         return false;
@@ -45,7 +49,7 @@ export function AuthProvider({ children }) {
     }
 
     return(
-        <AuthContext.Provider value={{user, login, logout}}>
+        <AuthContext.Provider value={{user,loading, login, logout}}>
             {children}
         </AuthContext.Provider>
     );
