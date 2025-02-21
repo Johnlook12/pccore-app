da9758fc81343f66480f8dbe621d092aa059085a
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-02-21 13:35:53 +0100

    feat: add products route

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ShopPage from './pages/ShopPage'
 import LoginPage from './pages/LoginPage'
 import ProtectedAdminRoute from "./components/ProtectedAdminRoute"
 import image403 from "./assets/img/403.jpg";
+import ProductsPage from './pages/ProductsPage';
 function App() {
 
   const Unauthorized = () => <div className='min-h-screen justify-items-center'>
@@ -22,6 +23,10 @@ function App() {
           <Route path='/login' element={<LoginPage formType={"login"} />}></Route>
           <Route path='/register' element={<LoginPage formType={"register"} />}></Route>
           <Route path='/' element={<Layout><HomePage /></Layout>}></Route>
+          <Route path='/products' element={
+            <ProtectedAdminRoute>
+              <Layout><ProductsPage></ProductsPage></Layout>
+            </ProtectedAdminRoute>}></Route>
           <Route path='/admin' element={
             <ProtectedAdminRoute>
               <Layout><AdminPanelPage /></Layout>
