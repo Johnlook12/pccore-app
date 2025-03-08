e428135f619ce7c4997c4a68baafa25f4a37e7a3
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-03-08 01:43:24 +0100

    feat: update UserDropDown to include admin panel link for administrators

--- src/components/UserDropDown.jsx
+++ src/components/UserDropDown.jsx
@@ -40,6 +40,7 @@ export default function UserDropDown() {
         )
     }
 
+
     return (
         <Menu as="div" className="relative inline-block text-left">
             <div>
@@ -54,22 +55,15 @@ export default function UserDropDown() {
                 className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
             >
                 <div className="py-1">
-                    <MenuItem>
-                        <a
-                            href="#"
+                    {user?.tipoUsuario.nombre === 'administrador' && (
+                        <MenuItem>
+                        <Link to="/admin"
                             className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
                         >
-                            Account settings
-                        </a>
+                            Panel administrador
+                        </Link>
                     </MenuItem>
-                    <MenuItem>
-                        <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                        >
-                            Support
-                        </a>
-                    </MenuItem>
+                    )}
                     {!user && <LoginItem />}
                     {user && <LogoutItem />}
                 </div>
