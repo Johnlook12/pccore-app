e14551a809de2e79758b20a3c90a9ed462549b67
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-03-08 01:29:59 +0100

    feat: add findById, update, delete methods

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,24 +1,80 @@
-import { useState, useEffect } from "react";
-import { findAll } from "../services/ProductoService";
+import { useState, useEffect} from "react";
+import { findAll, addProduct as addProductService, deleteProduct as deleteProductService, updateProduct as updateProductService, findById as findByIdService } from "../services/ProductoService";
 
 export const useProducts = () => {
     const [productos, setProductos] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const data = await findAll();
-                setProductos(data);
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
+    const fetchProducts = async () => {
+        try {
+            const data = await findAll();
+            setProductos(data);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
         }
+    }
+    useEffect(() => {
         fetchProducts();
     }, []);
 
-    return { productos, loading, error };
+    const addProduct = async (product) => {
+        setLoading(true);
+        setError('');
+        try {
+            const createdProduct = await addProductService(product);
+            fetchProducts();
+            return createdProduct;
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const findById = async (id) => {
+        setLoading(true);
+        setError('');
+        try {
+            const product = await findByIdService(id);
+            return product;
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const deleteProduct = async (id) => {
+        setLoading(true);
+        setError('');
+        try {
+            const deletedProduct = await deleteProductService(id);
+            fetchProducts();
+            return deletedProduct;
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+
+    const updateProduct = async (id, product) => {
+        setLoading(true);
+        setError('');
+        try {
+            const updatedProduct = await updateProductService(id, product);
+            fetchProducts();
+            return updatedProduct;
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { productos, findById, addProduct, updateProduct, deleteProduct, loading, error };
 }
